f107ef000650fc96df46dfb6ec8f98b95195b0a2
LancelotOzerny <lancelot.ozernuy@gmail.com>
2022-12-01 02:37:56 +0300

    Верстка дизайна сайта
    Реализовал базовую структуру html файла, которая содержит заголовок, textarea и 4 кнопки (по 1 на задание). Также создал и настроил стилли для сайта. Высоту textarea для удобства меняьт не стал.

---
+++ ex_4.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Задание 4</title>
+
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link rel="stylesheet" href="Styles/ex_4.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="page-title">Лабораторная работа номер 4</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<form class="page-form" method="Post" action="ex_4.php?go">
+					<div class="row">
+						<div class="col-md-12">
+							<textarea name="start-text" class="start-text" cols="30" rows="10"></textarea>
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="col-md-3">
+							<div class="exercise-btn-wrapper">
+								<input type="submit" class="exercise-btn" name="Задание 3" value="post">
+							</div>
+						</div>
+
+						<div class="col-md-3">
+							<div class="exercise-btn-wrapper">
+								<input type="submit" class="exercise-btn" name="Задание 6" value="post">
+							</div>
+						</div>
+
+						<div class="col-md-3">
+							<div class="exercise-btn-wrapper">
+								<input type="submit" class="exercise-btn" name="Задание 14" value="post">
+							</div>
+						</div>
+
+						<div class="col-md-3">
+							<div class="exercise-btn-wrapper">
+								<input type="submit" class="exercise-btn" name="Задание 18" value="post">
+							</div>
+						</div>
+					</div>
+				</form>		
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<div class="edited-text">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+</body>
+</html>
